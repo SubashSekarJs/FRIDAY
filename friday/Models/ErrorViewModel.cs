eac00a118dacef7a1e89e640b467e02345d41345
SubashSekarJs <subashsekar12@gmail.com>
2023-05-17 11:03:17 +0530

    initial commit

---
+++ friday/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace friday.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
